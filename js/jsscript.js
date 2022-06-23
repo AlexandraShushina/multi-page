9a6b0bd58c22ea1584cec9c91616b5afe5654ae6
AlexandraShushina <amshushina@edu.hse.ru>
2022-06-23 16:32:12 +0300

    Update jsscript.js

--- js/jsscript.js
+++ js/jsscript.js
@@ -33,53 +33,54 @@ $(document).ready(function(){
     $( ".rectangle_transparent_2" ).removeClass("flag_2");
     $( ".rectangle_transparent_2" ).addClass("flag_1");
   };
-});
-$(".rectangle_transparent_2").hover(function(){
+  });
+  $(".rectangle_transparent_2").hover(function(){
     $(".container_trees .tree_2").toggleClass("tree_2_green");
-});
-$(".container_trees .tree_2").hover(function(){
+  });
+  $(".container_trees .tree_2").hover(function(){
     $(".tree_2").toggleClass("tree_2_green");
-});
-$(".rectangle_transparent_3").on("click", function(){
-  if ($(".rectangle_transparent_3").hasClass("flag_1")) {
-    $(".container_trees .tree_3").css("z-index","200");
-    $( ".rectangle_transparent_3" ).removeClass("flag_1");
-    $( ".rectangle_transparent_3" ).addClass("flag_2");
-  };
-});
-$(".container_trees .tree_3").on("click", function(){
-  if ($(".rectangle_transparent_3").hasClass("flag_2")) {
-    $(".tree_3").css("z-index","1");
-    $( ".rectangle_transparent_3" ).removeClass("flag_2");
-    $( ".rectangle_transparent_3" ).addClass("flag_1");
-  };
-});
-$(".rectangle_transparent_3").hover(function(){
+  });
+  $(".rectangle_transparent_3").on("click", function(){
+    if ($(".rectangle_transparent_3").hasClass("flag_1")) {
+      $(".container_trees .tree_3").css("z-index","200");
+      $( ".rectangle_transparent_3" ).removeClass("flag_1");
+      $( ".rectangle_transparent_3" ).addClass("flag_2");
+    };
+  });
+  $(".container_trees .tree_3").on("click", function(){
+    if ($(".rectangle_transparent_3").hasClass("flag_2")) {
+      $(".tree_3").css("z-index","1");
+      $( ".rectangle_transparent_3" ).removeClass("flag_2");
+      $( ".rectangle_transparent_3" ).addClass("flag_1");
+    };
+  });
+  $(".rectangle_transparent_3").hover(function(){
     $(".container_trees .tree_3").toggleClass("tree_3_green");
-});
-$(".container_trees .tree_3").hover(function(){
+  });
+  $(".container_trees .tree_3").hover(function(){
     $(".container_trees .tree_3").toggleClass("tree_3_green");
-});
-$(".rectangle_transparent_4").on("click", function(){
-  if ($(".rectangle_transparent_4").hasClass("flag_1")) {
-    $(".container_trees .tree_4").css("z-index","200");
-    $( ".rectangle_transparent_4" ).removeClass("flag_1");
-    $( ".rectangle_transparent_4" ).addClass("flag_2");
-  };
-});
-$(".container_trees .tree_4").on("click", function(){
-  if ($(".rectangle_transparent_4").hasClass("flag_2")) {
-    $(".container_trees .tree_4").css("z-index","2");
-    $( ".rectangle_transparent_4" ).removeClass("flag_2");
-    $( ".rectangle_transparent_4" ).addClass("flag_1");
-  };
-});
-$(".rectangle_transparent_4").hover(function(){
-  $(".container_trees .tree_4").toggleClass("tree_4_green");
-});
-$(".container_trees .tree_4").hover(function(){
-  $(".container_trees .tree_4").toggleClass("tree_4_green");
-});
+  });
+  $(".rectangle_transparent_4").on("click", function(){
+    if ($(".rectangle_transparent_4").hasClass("flag_1")) {
+      $(".container_trees .tree_4").css("z-index","200");
+      $( ".rectangle_transparent_4" ).removeClass("flag_1");
+      $( ".rectangle_transparent_4" ).addClass("flag_2");
+    };
+  });
+  $(".container_trees .tree_4").on("click", function(){
+    if ($(".rectangle_transparent_4").hasClass("flag_2")) {
+      $(".container_trees .tree_4").css("z-index","2");
+      $( ".rectangle_transparent_4" ).removeClass("flag_2");
+      $( ".rectangle_transparent_4" ).addClass("flag_1");
+    };
+  });
+  $(".rectangle_transparent_4").hover(function(){
+    $(".container_trees .tree_4").toggleClass("tree_4_green");
+  });
+  $(".container_trees .tree_4").hover(function(){
+    $(".container_trees .tree_4").toggleClass("tree_4_green");
+  });
+
 // Анимация страницы экскурсии
   $(".rectangle_long_2").on("click", function(){
     if ($(this).hasClass("flag_1")) {
@@ -115,6 +116,7 @@ $(".container_trees .tree_4").hover(function(){
   $(".tree_4_long").hover(function(){
     $(".tree_4_long").toggleClass("tree_4_long_green");
   });
+
   // Error404
   $(".rectangle_error_1").on("click", function(){
     if ($(this).hasClass("flag_1")) {
@@ -236,6 +238,9 @@ $(".container_trees .tree_4").hover(function(){
     $('#description_4').append(right);
     $('#description_6').append(left);
      return false;
+  });
+  $(".events_tree").hover(function(){
+      $(".events_tree").toggleClass("events_tree_green");
   });
 // Веб-постер
   $( function() {
